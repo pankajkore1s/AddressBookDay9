ece6acebaa9f4718c204d33dd609a70ba110f939
pankaj kore <korep95@gmail.com>
2022-02-04 18:47:36 +0530

    [pankaj]added welcome message

---
+++ src/com/bridgeLabz/AddressBookDay9/AddressBook.java
@@ -0,0 +1,7 @@
+package com.bridgeLabz.AddressBookDay9;
+
+public class AddressBook {
+	public static void main (String []args) {
+		System.out.println("****Welcome To The Address Book****");
+	}
+}
