4de679554db6310f42c9ebe0031c3346de594919
pankaj kore <korep95@gmail.com>
2022-02-05 16:33:37 +0530

    [pankaj]added feature of creating contact

---
+++ src/com/bridgeLabz/AddressBookDay9/AddressBookUC1.java
@@ -0,0 +1,9 @@
+package com.bridgeLabz.AddressBookDay9;
+
+public class AddressBookUC1 {
+	public static void main (String []args) {
+		Contacts person1=new Contacts();
+		person1.credientials();
+		person1.printCredientials();
+	}
+}
---
+++ src/com/bridgeLabz/AddressBookDay9/Contacts.java
@@ -0,0 +1,33 @@
+package com.bridgeLabz.AddressBookDay9;
+
+import java.util.*;
+
+class Contacts {
+	public  String firstName,lastName,address,city,state,zip,phoneNumber,emailID;
+	public void credientials() {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter First Name=");
+		String firstName=sc.nextLine();
+		System.out.println("Enter Last Name=");
+		String lastName=sc.nextLine(); 
+		System.out.println("Enter Address=");
+		String address=sc.nextLine();
+		System.out.println("Enter city=");
+		String city=sc.nextLine();
+		System.out.println("Enter State=");
+		String state=sc.nextLine();
+		System.out.println("Enter zip code=");
+		int zip=sc.nextInt();
+		System.out.println("Enter phone number=");
+		int phoneNumber=sc.nextInt();
+		System.out.println("Enter EmailId=");
+		String emailID=sc.nextLine();
+		sc.close();
+		
+	}
+	public void printCredientials() {
+		System.out.print("FirstName : "+firstName+"\n"+"LastName : "+lastName+"\n"+"Address :"+address+"\n"+
+            	"City : "+city+"\n"+"State : "+state+"\n"+"Zip : "+zip+"\n"+
+		        "Phone Number : "+phoneNumber+"\n"+"Email : "+emailID);
+	}
+}
